f0a5d45f1ad818fadbe41ff6a27f7dd0d6279669
jittaearn <jitta.k@ku.th>
2021-01-26 15:56:59 +0700

    fis static

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = '/home/jittaearn/myPortfolio/static'
+
+
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
